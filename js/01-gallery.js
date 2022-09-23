47c8cc6d5c2342172affeb49903a2cc3761a2554
Betelgeuse22 <Don.vitaki@gmail.com>
2022-09-23 22:11:02 +0200

    Update task 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallary = document.querySelector(".gallery");
+
+const markup = galleryItems.reduce(
+  (acc, item) =>
+    acc +
+    `<div class="gallery__item">
+        <a class="gallery__link" href=${item.original}>
+            <img
+                class="gallery__image"
+                src=${item.preview}
+                data-source=${item.original}
+                alt=${item.description}
+            />
+        </a>
+    </div>`,
+  ""
+);
+
+gallary.insertAdjacentHTML("beforeend", markup);
+
+gallary.addEventListener("click", onClickImage);
+
+function onClickImage(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<div class="modal">
+        <img
+            src=${evt.target.dataset.source}
+        />
+      </div>`
+  );
+
+  instance.show();
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      instance.close();
+    }
+  });
+}
